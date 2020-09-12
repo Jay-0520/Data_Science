459ed5a6ba338a36790a1cdf216c9c3733806389
JINGJING HUANG <jingjinghuang@JINGJINGs-MacBook-Pro.local>
2020-09-12 14:28:14 -0400

    Add a summary note

---
+++ project_1/notes_pandas.py
@@ -0,0 +1,20 @@
+
+
+# a while loop to loop through each item in the list
+i=0
+while( i != len(x) ):
+    print(x[i])
+    i = i + 1
+
+# Use * to repeat lists
+[1]*3
+
+# check if something is inside a list
+
+1 in [1, 2, 3]
+
+# Unpack a sequence into different variables
+
+x = ('Christopher', 'Brooks', 'brooksch@umich.edu')
+fname, lname, email = x
+
